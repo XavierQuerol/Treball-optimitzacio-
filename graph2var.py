11e77c6a80efaadf328f97d721ced170df01ce46
XavierQuerol <101579003+XavierQuerol@users.noreply.github.com>
2023-01-09 23:30:13 +0100

    Update graph2var.py

--- graph2var.py
+++ graph2var.py
@@ -21,8 +21,8 @@ ineq_3 = 7*x + 3*y <= 60
 ineq_4 = y <= 20 + 0*x
 ineq_5 = x <= 8 + 0*y
 
-plt.imshow( (ineq_1 & ineq_2 & ineq_3 & ineq_4 & ineq_5).astype(int) , 
-                extent=(x.min(),x.max(),y.min(),y.max()),origin="lower", cmap="Greys", alpha = 0.3);
+#plt.imshow( (ineq_1 & ineq_2 & ineq_3 & ineq_4 & ineq_5).astype(int) , 
+                #extent=(x.min(),x.max(),y.min(),y.max()),origin="lower", cmap="Greys", alpha = 0.3);
 
 # plot the lines defining the constraints
 x = np.linspace(-600, 600, 2000)
@@ -34,15 +34,15 @@ y4 = 20 + 0*x
 x1 = 8 + 0*y
 
 # Make plot
-plt.plot(x, y1, label=r'Inequality 1')
-plt.plot(x, y2, label=r'Inequality 2')
-plt.plot(x, y3,label=r'Inequality 3')
-plt.plot(x, y4,label=r'Inequality 4')
-plt.plot(x1, y,label=r'Inequality 5')
+plt.plot(x, y1, label=r'2*Y + 1*K <= 100')
+plt.plot(x, y2, label=r'1*Y +  1*K <= 25')
+plt.plot(x, y3,label=r'7*Y + 3*K <= 60')
+plt.plot(x, y4,label=r'K <= 20')
+plt.plot(x1, y,label=r'Y <= 8')
 
 plt.xlim(0,100)
 plt.ylim(0,100)
-plt.legend(bbox_to_anchor=(0.5, 0.95), loc=2, borderaxespad=0.)
+plt.legend(bbox_to_anchor=(0.35, 0.95), loc=2, borderaxespad=0.)
 plt.xlabel(r'$Neu$')
 plt.ylabel(r'$Figures$')
 
